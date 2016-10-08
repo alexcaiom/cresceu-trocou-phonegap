b5ede1153c18413c03c440cbd3e0cfa1c8f88eee
Alex <alex@waiso.com.br>
2016-10-08 09:58:46 -0300

    Primeiro commit

---
+++ kidz/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,19 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-device": "1.1.3",
+    "cordova-plugin-console": "1.0.4"
+};
+// BOTTOM OF METADATA
+});
